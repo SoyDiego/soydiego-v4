ee13c0bb5d685f085daba23f231e67806a45e441
Diego <diegofranchina@gmail.com>
2023-07-23 14:44:15 +0200

    Contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { allContentEN } from 'src/shared/data';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  contact = allContentEN.contactMe;
+
+  handleGoToTop() {
+    console.log('test');
+    window.scrollTo(0, 0);
+  }
 
 }
